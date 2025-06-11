fd8fb66d83fb60537930de4401befa289cf3f8b9
constantpi <wada.zkai@gmail.com>
2025-06-12 02:09:30 +0900

    Qudit間の線形写像の集合がベクトル空間をなす

--- QuantumInfo/Finite/Hilbert.lean
+++ QuantumInfo/Finite/Hilbert.lean
@@ -49,12 +49,33 @@ def DensityOperator (Qudit : Type) [QuditType Qudit] : Type :=
   { f : Qudit →ₗ[ℂ] Qudit // Continuous f ∧ ∀ x y : Qudit, ⟪f.1 x, y⟫ = ⟪x, f.1 y⟫ ∧ ∀ x: Qudit, 0 ≤  ⟪f.1 x, x⟫.re
   ∧ LinearMap.trace ℂ Qudit f = 1 }
 
-noncomputable instance : AddCommMonoid DensityOperator :=
-  interInstance
+-- QuditType クラスの型 Qudit を受け取り、Qudit から Qudit への線形写像の集合を返す関数
+def LinearOperatorSet (Qudit : Type) [QuditType Qudit] : Type :=
+  Qudit →ₗ[ℂ] Qudit
 
--- 量子チャンネルの定義
-def QuantumChannel : Type :=
-  { f : DensityOperator →ₗ[ℂ] DensityOperator // Continuous f}
+
+
+-- AddCommMonoid のインスタンスを定義
+-- instance (Qudit : Type) [QuditType Qudit] : AddCommMonoid (LinearOperatorSet Qudit) where
+--   add := sorry
+--   zero := sorry
+
+-- AddCommMonoid のインスタンスを定義
+instance (Qudit : Type) [QuditType Qudit] : AddCommMonoid (LinearOperatorSet Qudit) :=
+  LinearMap.addCommMonoid
+
+-- module のインスタンスを定義
+instance (Qudit : Type) [QuditType Qudit] : Module ℂ (LinearOperatorSet Qudit) :=
+  LinearMap.module
+
+
+def QuantumChannel (H_in H_out : Type) [QuditType H_in] [QuditType H_out] : Type :=
+  { φ : LinearOperatorSet H_in →ₗ[ℂ] LinearOperatorSet H_out //
+  -- トレースを保存する
+  ∀ (f: LinearOperatorSet H_in), LinearMap.trace ℂ H_in f = LinearMap.trace ℂ H_out (φ f)
+  -- 完全正定値写像
+  }
+
 
   -- TODO:測定とチャンネル(QuantumChannel)
   -- operatorをoperatorに移す線形写像
